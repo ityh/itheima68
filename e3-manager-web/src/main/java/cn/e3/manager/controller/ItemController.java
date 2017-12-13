528a5bfc3235340c28fedc268a5207a5115b2d41
mutou <mutou@baba>
2017-12-13 10:14:36 +0800

    第二次提交验证

---
+++ e3-manager-web/src/main/java/cn/e3/manager/controller/ItemController.java
@@ -0,0 +1,27 @@
+package cn.e3.manager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.pojo.TbItem;
+
+@Controller
+public class ItemController {
+	// 注入service服务对象
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("item/list/{itemId}")
+	@ResponseBody
+	public TbItem findItemByID(@PathVariable Long itemId) {
+		// 调用service服务方法
+		TbItem item = itemService.findItemByID(itemId);
+
+		return item;
+	}
+
+}
